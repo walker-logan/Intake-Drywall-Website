e96fd93857b79ed4258e300139ade3fc4cad7f21
Walker Logan <walker.logan44@gmail.com>
2023-04-25 19:08:50 -0500

    added animations and did more styling. changed the nav bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,81 @@
-// Wait for the page to load
-window.addEventListener('load', function() {
-  // Wait for the "Intake Drywall" animation to complete
-  setTimeout(function() {
-    // Add the "content-visible" class to fade in the content
-    document.getElementById('hero').classList.add('hero-visible');
-  }, 2000); // 2000 milliseconds = 2 seconds
-});
+// main visual typewriter animation
+
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
+
+// scrolling effect on header
+function parallaxEffect1() {
+  var speed = 0.5;
+  var header = document.querySelector("header");
+  var windowYOffset = window.pageYOffset;
+  var bgPosition = "50% " + windowYOffset * speed + "px";
+  header.style.backgroundPosition = bgPosition;
+}
+window.addEventListener("scroll", parallaxEffect1);
+// scrolling effect on music section
+function parallaxEffect2() {
+  var speed = 0.5;
+  var musicSection = document.querySelector('.music-section')
+  var windowYOffset = window.pageYOffset;
+  var bgPosition = "50% " + (windowYOffset * speed) + "px";
+  musicSection.style.backgroundPosition = bgPosition;
+}
+window.addEventListener("scroll", parallaxEffect2)
+
+
